b4b4ad6bce842777c266d2b902bf6ef0028bdcfd
Danil Karmanov <KarmanovDanil@gmail.com>
2019-06-11 19:57:38 +0500

    Создан проект "Песочница"

---
+++ sandbox/src/main/java/MyFirstApp.java
@@ -0,0 +1,14 @@
+/*
+Это пример простой программы на Java.
+Присвойте файлу с исходным кодом имя Example.java.
+*/
+
+public class MyFirstApp {
+  // Выполнение любой программы на Java начинается
+  // с вызова метода main()
+  public static void main (String[] args) {
+    System.out.println ("Я управляю");
+    System.out.println("миром");
+    System.out.println("как никто");
+  }
+}
