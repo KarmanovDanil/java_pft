0719d18472ae7f5e330bb4a29a8dd767107abf53
Danil Karmanov <KarmanovDanil@gmail.com>
2019-06-13 13:07:49 +0500

    Добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstApp.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstApp.java
@@ -7,9 +7,16 @@ public class MyFirstApp {
   // Выполнение любой программы на Java начинается
   // с вызова метода main()
   public static void main (String[] args) {
-    System.out.println ("Я управляю");
-    System.out.println("миром");
-    System.out.println("как никто");
+    hello("world");
+    hello("user");
+    hello("Danil");
+
+    double len = 5;
+    System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
 
     // Выражения и операции
     System.out.println(2 + 2);
@@ -36,10 +43,25 @@ public class MyFirstApp {
     //Литеральные значения - явно описаны в коде программы
     //Вычислимые значения - результат вычисления какого-то выражения (склеивание строк или ариф.операция)
     //Переменная - идентификатор, который ссылается на значение, хранящиеся в памяти компьютера
-    int l = 6;
-    int s = l * l;
+    double l = 6.0;
+    double s = l * l;
     System.out.println("Площадь квадрата со стороной " + l + " = " + s);
 
 
+
   }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+
+  }
+
+  public static double area (double r) {
+    return  r * r;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
+  }
+
 }
